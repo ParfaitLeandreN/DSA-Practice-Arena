c74a83ce3253db1b6024d602620e0c1cdbad2e59
Parfait Leandre Ndagijimana <parfaitleandrendagijimana@gmail.com>
2025-12-03 18:35:33 -0500

    Time: 23 ms (20.49%), Space: 31.6 MB (53.63%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -12,9 +12,21 @@ class Solution:
         # return False
 
 
-        ## SORTING
-        nums.sort()
-        for i in range(len(nums) - 1):
-            if nums[i] == nums[i+1]:
-                return True
-        return False
+        # ## SORTING
+        # nums.sort()
+        # for i in range(len(nums) - 1):
+        #     if nums[i] == nums[i+1]:
+        #         return True
+        # return False
+
+
+        # ## BRUTE FORCE
+        # for i in range(len(nums)):
+        #     for j in range(i+1, len(nums)):
+        #         if nums[i] == nums[j]:
+        #             return True
+        # return False
+
+
+        ## HASHSET LENGTH
+        return len(set(nums)) != len(nums)
